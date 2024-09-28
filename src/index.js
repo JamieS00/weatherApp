939fe5ba9f8592565e194d2e5a66bfbc15629d0d
Jamie Singleton <43870204+JamieS00@users.noreply.github.com>
2024-09-28 09:43:54 -0400

    change func name (updateWeather) , comments

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 /*Goal: display the temp for city */
-displayTemp = (response) => {
+updateWeather = (response) => {
   let currentAPItemp = Math.round(response.data.temperature.current);
   //   console.log(currenttemp); //testing purposes
 
@@ -7,12 +7,12 @@ displayTemp = (response) => {
   temp.innerHTML = currentAPItemp;
 };
 
-/*Goal: make api call & update UI based on city*/
+/*Goal: make api call & calls updateWeather func*/
 searchCity = (city) => {
   let apiKey = "4ddbb61eb5o419b8d734a63d7f1t0b56";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   //   console.log(apiUrl); //testing purposes
-  axios.get(apiUrl).then(displayTemp); //api call
+  axios.get(apiUrl).then(updateWeather); //api call
 };
 
 /*This is called every time user clicks submit form*/
