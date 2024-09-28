a90eb03dd33f7ccd2ee0473395f1cc9f0c286a8d
Jamie Singleton <43870204+JamieS00@users.noreply.github.com>
2024-09-27 22:07:30 -0400

    added code for displaycity & search city, created temp function

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
+refreshWeather = (response) => {};
+
+/*Goal:make api call & update UI based on city*/
+searchCity = (city) => {
+  let apiKey = "4ddbb61eb5o419b8d734a63d7f1t0b56";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  //   console.log(apiUrl); //testing purposes
+  axios.get(apiUrl).then(refreshWeather); //api call
+};
+
 /*Goal: display city(h1) based on user input*/
 displayCityName = (event) => {
   event.preventDefault(); //stop page from reloading
   let cityName = document.querySelector(".h1-city-name");
   let Input = document.querySelector(".search-input");
-
   cityName.innerHTML = Input.value;
+
+  //calls funct , city = name user enters
+  searchCity(Input.value);
 };
 let searchElement = document.querySelector("#search-form");
 searchElement.addEventListener("submit", displayCityName);
