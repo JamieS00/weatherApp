53f35660bb428de06b9a75123c17455d3df2e263
Jamie Singleton <43870204+JamieS00@users.noreply.github.com>
2024-09-27 21:38:04 -0400

    removing test console logs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 /*Goal: display city(h1) based on user input*/
 displayCityName = (event) => {
-  console.log("inside");
   event.preventDefault(); //stop page from reloading
   let cityName = document.querySelector(".h1-city-name");
   let Input = document.querySelector(".search-input");
@@ -9,4 +8,3 @@ displayCityName = (event) => {
 };
 let searchElement = document.querySelector("#search-form");
 searchElement.addEventListener("submit", displayCityName);
-console.log("come onnnn");
